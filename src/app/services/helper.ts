b8f093ea7e487b62d3b82bc10eada34b2308c8bf
vishalsin888 <vishalsin888@gmail.com>
2021-10-03 20:29:50 +0530

    initial commit

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,4 +1,5 @@
 // let baseUrl="http://localhost:8080"
-let baseUrl="http://localhost:9000"
+// let baseUrl="http://localhost:9000"
+let baseUrl="https://inpariksha.herokuapp.com";
 
 export default  baseUrl;
