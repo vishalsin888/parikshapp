3a545c50336628ce2dac6159fb15148e39bb1d85
vishalsin888 <81374203+vishalsin888@users.noreply.github.com>
2021-10-05 02:24:18 +0530

    Update helper.ts

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,5 +1,4 @@
 // let baseUrl="http://localhost:8080"
 // let baseUrl="http://localhost:9000"
-let baseUrl="https://inpariksha.herokuapp.com";
-
+let baseurl = "https://parikshaserver.herokuapp.com";
 export default  baseUrl;
